741838ac31921047a4df72f82b269cff2f2d45ad
Paul Hammant <paul@hammant.org>
2020-07-24 15:32:34 +0100

    first commit

---
+++ src/test/java/ggg/UnitTest.java
@@ -0,0 +1,19 @@
+package ggg;
+
+import io.jooby.MockRouter;
+import io.jooby.StatusCode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTest {
+  @Test
+  public void shouldSayWelcome() {
+    MockRouter router = new MockRouter(new App());
+    router.get("/", rsp -> {
+      assertEquals("Welcome to Jooby!", rsp.value());
+      assertEquals(StatusCode.OK, rsp.getStatusCode());
+    });
+
+  }
+}
